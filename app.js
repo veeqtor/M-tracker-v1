7671b6fd321af74e7f272258f2b72504500ff487
veeqtor <nwokeochavictor@gmail.com>
2018-05-16 15:18:52 +0100

    [Finished #157554696] Get all the requests for a logged in user

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 import bodyParser from 'body-parser';
 
+import requestRoute from './api/routes/requestRoute';
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded());
+
+app.use('/api/v1/users/requests', requestRoute);
 
 export default app;
--- dist/app.js
+++ dist/app.js
@@ -12,9 +12,19 @@ var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
+var _requestRoute = require('./api/routes/requestRoute');
+
+var _requestRoute2 = _interopRequireDefault(_requestRoute);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
+
+app.use(_bodyParser2.default.urlencoded());
+
+app.use('/api/v1/users/requests', _requestRoute2.default);
 
 exports.default = app;
 //# sourceMappingURL=app.js.map
